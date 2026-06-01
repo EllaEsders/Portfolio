c9751ed209cc85ca27013050fa092130d615644c
EllaEsders <ellae@vt.edu>
2026-06-01 13:17:52 -0400

    fixing routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import '../styles/Navbar.css'
-
-function Navbar(){
-   return(
+import { Link } from "react-router-dom";
+export default function Navbar() {
+   return (
       <nav className="navbar">
          <h2>Portfolio</h2>
 
          <div className="links">
-            <a href="/">Home</a>
-            <a href="/ProjectsPage">Projects</a>
+            <Link to="/">Home</Link>
+            <Link to="/ProjectsPage">Projects</Link>
          </div>
       </nav>
-   )
+   );
 }
 
-export default Navbar
+// export default Navbar
